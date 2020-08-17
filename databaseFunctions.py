cd8d69dfe85eaf5619e856d4c493f4dfc7405c24
Elkady98 <57726466+Elkady98@users.noreply.github.com>
2020-08-17 23:06:01 +0200

    Update databaseFunctions.py

--- databaseFunctions.py
+++ databaseFunctions.py
@@ -7,3 +7,14 @@ DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'database.sqlite3')
 def db_connect(db_path=DEFAULT_PATH):
     conn = sqlite3.connect(db_path)
     return conn
+
+def create_table(conn):
+    c = conn.cursor()
+    sql = """ CREATE TABLE IF NOT EXISTS user (
+                                     username text NOT NULL PRIMARY KEY,
+                                     password text NOT NULL,
+                                     gender char NOT NULL
+                                 ); """
+    c.execute(sql)
+
+create_table(db_connect())
