c5a3c98b75b0e573b15bf8c4178e962919a6e934
Shehab Kotb <shehabktob@gmail.com>
2020-08-17 23:41:12 +0200

    rivebot

---
+++ riveBot.py
@@ -0,0 +1,14 @@
+from rivescript import RiveScript
+bot = RiveScript(utf8=True)
+bot.load_directory("brain")#brain
+bot.sort_replies()
+
+
+def chat(user_id, message):
+    if message == '':
+        return -1, {"message": "No Message to response"}
+    else:
+        response = bot.reply(str(user_id), message)
+    if response == "":
+        return -1, {"message": "No Message to response"}
+    return 0, response
