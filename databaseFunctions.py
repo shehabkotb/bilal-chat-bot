5da47cb4647d7a03e55d2867dacf367c15aa551f
Elkady98 <57726466+Elkady98@users.noreply.github.com>
2020-08-17 23:06:48 +0200

    Update databaseFunctions.py

--- databaseFunctions.py
+++ databaseFunctions.py
@@ -18,3 +18,11 @@ def create_table(conn):
     c.execute(sql)
 
 create_table(db_connect())
+
+def add_user(conn, user_data):
+    sql = ''' INSERT INTO user(username,password,gender)
+              VALUES("{}","{}","{}") '''.format(user_data['username'],user_data['password'],user_data['gender'])
+    cur = conn.cursor()
+    cur.execute(sql, user_data)
+    return cur.lastrowid
+
